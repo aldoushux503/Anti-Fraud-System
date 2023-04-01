7db6df052e8e80a7135dbe2fb07cbf502e88bb82
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-01 15:28:04 +0200

    Initial commit

--- src/main/java/com/example/antifraudsystem/AntiFraudSystemApplication.java
+++ src/main/java/com/example/antifraudsystem/AntiFraudSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AntiFraudSystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AntiFraudSystemApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AntiFraudSystemApplication.class, args);
+	}
 
 }
