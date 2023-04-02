9620a7fbefecbfe5ead41f881319858ed1eb8440
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-02 20:11:05 +0200

    Convert Transaction class to record and add logic to endpoint /transaction

--- src/main/java/com/example/antifraudsystem/Transaction.java
+++ src/main/java/com/example/antifraudsystem/Transaction.java
@@ -1,13 +1,4 @@
 package com.example.antifraudsystem;
 
-public class Transaction {
-    private final int amount;
-
-    public Transaction(int amount) {
-        this.amount = amount;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
+public record Transaction(int amount) {
 }
--- src/main/java/com/example/antifraudsystem/ValidationController.java
+++ src/main/java/com/example/antifraudsystem/ValidationController.java
@@ -5,15 +5,28 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class ValidationController {
 
     @PostMapping("/api/antifraud/transaction")
-    public void validateAmount(@RequestBody String requestJson) throws JsonProcessingException {
+    public Map<String, String> validateAmount(@RequestBody String requestJson) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
+        Transaction transaction = mapper.readValue(requestJson, Transaction.class);
 
+        Map<String, String> res = new HashMap<>();
+        if (transaction.amount() <= 200) {
+            res.put("amount", "ALLOWED");
+        } else if (transaction.amount() <= 1500) {
+            res.put("amount", "MANUAL_PROCESSING");
+        } else {
+            res.put("amount", "PROHIBITED");
+        }
+
+        return res;
     }
 }
