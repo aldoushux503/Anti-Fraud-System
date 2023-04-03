fe6f9b1d0f56f4ea03ac05a08c12837f150c7f86
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-03 11:29:30 +0200

    Replaced amount type to long and edited validateAmount() method

---
+++ src/main/java/com/example/antifraudsystem/TransactionService.java
@@ -0,0 +1,4 @@
+package com.example.antifraudsystem;
+
+public record TransactionService(long amount) {
+}
--- src/main/java/com/example/antifraudsystem/ValidationController.java
+++ src/main/java/com/example/antifraudsystem/ValidationController.java
@@ -1,26 +1,18 @@
 package com.example.antifraudsystem;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class ValidationController {
 
     @PostMapping("/api/antifraud/transaction")
-    public Map<String, String> validateAmount(@RequestBody String requestJson) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        Transaction transaction = mapper.readValue(requestJson, Transaction.class);
-
+    public Map<String, String> validateAmount(@RequestBody TransactionService transaction) {
         if (transaction.amount() <= 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
