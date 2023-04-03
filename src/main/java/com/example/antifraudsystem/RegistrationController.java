896b0163d17903e132929e10f0dc2d7ad1c06532
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-03 14:32:54 +0200

    Added new registrationController and SecurityConfiguration classes

---
+++ src/main/java/com/example/antifraudsystem/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.example.antifraudsystem;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @PostMapping("/api/auth/user")
+    public void registrationUser() {
+        
+    }
+}
