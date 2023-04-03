280b95fdae796b01ded8f1fcc63e0a1006e4d0a6
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-03 11:12:22 +0200

    Replaced Map<String, String> to Map.of()

--- src/main/java/com/example/antifraudsystem/ValidationController.java
+++ src/main/java/com/example/antifraudsystem/ValidationController.java
@@ -2,10 +2,13 @@ package com.example.antifraudsystem;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,15 +21,16 @@ public class ValidationController {
         ObjectMapper mapper = new ObjectMapper();
         Transaction transaction = mapper.readValue(requestJson, Transaction.class);
 
-        Map<String, String> res = new HashMap<>();
-        if (transaction.amount() <= 200) {
-            res.put("amount", "ALLOWED");
-        } else if (transaction.amount() <= 1500) {
-            res.put("amount", "MANUAL_PROCESSING");
-        } else {
-            res.put("amount", "PROHIBITED");
+        if (transaction.amount() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        return res;
+        if (transaction.amount() <= 200) {
+            return Map.of("result", "ALLOWED");
+        } else if (transaction.amount() <= 1500) {
+            return Map.of("result", "MANUAL_PROCESSING");
+        } else {
+            return Map.of("result", "PROHIBITED");
+        }
     }
 }
