d6f2e9a3666522a91835d24c0b5b947290e6222f
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-03 16:41:23 +0200

    Added logic to UserDetailsImpl class and add new class UserDetailsServiceImpl

--- src/main/java/com/example/antifraudsystem/UserDetailsImpl.java
+++ src/main/java/com/example/antifraudsystem/UserDetailsImpl.java
@@ -1,43 +1,56 @@
 package com.example.antifraudsystem;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> rolesAndAuthorities;
+
+
+    public UserDetailsImpl(User user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.rolesAndAuthorities = List.of(new SimpleGrantedAuthority(user.getRole()));
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return rolesAndAuthorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
