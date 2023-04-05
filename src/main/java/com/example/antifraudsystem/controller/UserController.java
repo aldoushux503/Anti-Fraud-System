065e89a05738f7f38b445880a30f059d10dceb9a
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-05 11:34:42 +0200

    Added a UserController class

---
+++ src/main/java/com/example/antifraudsystem/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.antifraudsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+
+}
