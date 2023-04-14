37f19b1cb7b272ae89deb3917a17c664dadfccb6
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-14 16:14:05 +0200

    Added new enum RegionCode and new fields to Transaction class

--- src/main/java/com/example/antifraudsystem/entity/Transaction.java
+++ src/main/java/com/example/antifraudsystem/entity/Transaction.java
@@ -1,24 +1,47 @@
 package com.example.antifraudsystem.entity;
 
 
+import com.example.antifraudsystem.enums.RegionCode;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "transactions")
 public class Transaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     @Min(1)
+    @Column
     private long amount;
 
     @NotBlank
+    @Column
     private String ip;
 
     @NotBlank
+    @Column
     private String number;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private RegionCode region;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date date;
 }
---
+++ src/main/java/com/example/antifraudsystem/enums/RegionCode.java
@@ -0,0 +1,5 @@
+package com.example.antifraudsystem.enums;
+
+public enum RegionCode {
+    EAP, ECA, HIC, LAC, MENA, SA, SSA
+}
