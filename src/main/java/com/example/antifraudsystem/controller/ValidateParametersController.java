c4f04bdece759c55db6b0924c78cc6d1ca3b5583
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-14 17:42:02 +0200

    Added ValidationParametersController

---
+++ src/main/java/com/example/antifraudsystem/controller/ValidateParametersController.java
@@ -0,0 +1,20 @@
+package com.example.antifraudsystem.controller;
+
+
+import jakarta.validation.ValidationException;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class ValidateParametersController  {
+
+    @ResponseBody
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> exceptionHandler() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+}
